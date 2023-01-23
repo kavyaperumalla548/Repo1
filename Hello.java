f3c033265085a99dc7680f7aa7b690bd37cd4f78
kavyaperumalla548 <72217031+kavyaperumalla548@users.noreply.github.com>
2023-01-23 10:33:04 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-import java.io.*;
-public static void main(String args[])
-{
-System.out.println("Hello World");
+class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
